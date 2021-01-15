af0f53d381609a6b6dee770065c4d6230c419ab0
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-15 17:47:10 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-echo
-echo "## $1"
-echo
+
+sudo -E iptables -I INPUT -p tcp --dport 9001 -j ACCEPT
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
+sudo -E yum install -y nodejs wget
+git clone --branch master https://github.com/ether/etherpad-lite.git
+cd etherpad-lite
+npm install ep_hash_auth bcrypt argon2
+npm install adminpads2 author_hover message_all pad_activity_nofication_in_title padlist2 show_whitespace
+bin/run.sh
