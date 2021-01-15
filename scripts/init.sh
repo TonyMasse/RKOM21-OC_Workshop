6b97bf525304cb11c605fe627cd6da104622680e
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-15 17:48:08 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo -E yum install -y nodejs wget
 git clone --branch master https://github.com/ether/etherpad-lite.git
 cd etherpad-lite
+wget https://raw.githubusercontent.com/TonyMasse/RKOM21-OC_Workshop/main/config/settings.json
 npm install ep_hash_auth bcrypt argon2
 npm install adminpads2 author_hover message_all pad_activity_nofication_in_title padlist2 show_whitespace
 bin/run.sh
