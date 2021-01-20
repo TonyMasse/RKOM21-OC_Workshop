0fc328f67d53ac5d1eabd57b0a2bbfaa2c8bc5b6
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-20 22:07:49 +0000

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -43,7 +43,7 @@ curl -sL https://raw.githubusercontent.com/TonyMasse/RKOM21-OC_Workshop/main/con
 for i in {22,28,34,40} ; do echo -en "\e[38;5;${i}m#\e[0m" ; done
 echo -e "\e[92m# Install cryptographic and authentation modules for \e[1metherpad\e[39m\e[25m"
 
-npm install ep_hash_auth ep_bcrypt ep_argon2
+npm install ep_hash_auth bcrypt argon2
 npm install ep_adminpads2 ep_author_hover ep_message_all
 sudo iptables -L -n | grep 9001
 
