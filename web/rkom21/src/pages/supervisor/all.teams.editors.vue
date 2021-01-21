d6fee7000b23110747ddc707dc665822f5ef6c73
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-21 16:40:34 +0000

    Supervisor dashboard

--- web/rkom21/src/pages/supervisor/all.teams.editors.vue
+++ web/rkom21/src/pages/supervisor/all.teams.editors.vue
@@ -1,13 +1,16 @@
 <template>
   <q-page class="bg-grey-10 text-grey-5">
-    <!-- <iframe style="width: 100%; height: calc(100vh - 25px)" :src="editorUrl" frameborder="0"></iframe>
-    <iframe style="width: 100%; height: calc(100vh - 25px)" :src="editorUrl" frameborder="0"></iframe> -->
-    <div class="column col-md-auto q-gutter-xs" v-for="team in teams" :key="team.code">
-      <div class="q-pa-sm">
-        A
-      </div>
-      <div class="q-pa-sm">
-        B
+    <div class="row justify-center q-gutter-none">
+      <div class="column col-md-auto q-gutter-none" v-for="team in teams" :key="team.code" :style="'width: ' + 100 / teams.length + '%;'">
+        <div class="text-center text-bold">
+          Team <span style="text-decoration: underline;">{{ team.name.substring(0, 1) }}</span>{{ team.name.substring(1) }}
+        </div>
+        <div class="bg-grey-8">
+          <iframe style="width: 100%; height: calc(50vh - 25px)" :src="team.editorBaseUrl + '/p/' + team.code + '_is_mistnet.jq'" frameborder="1"></iframe>
+        </div>
+        <div class="bg-grey-8">
+          <iframe style="width: 100%; height: calc(50vh - 25px)" :src="team.editorBaseUrl + '/p/' + team.code + '_mistnet.jq'" frameborder="1"></iframe>
+        </div>
       </div>
     </div>
   </q-page>
