3e5cb066f2c6ff927f0f08870dc4bc57c321cd34
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-25 16:54:11 +0000

    Update mistnet.sample.drip.pl

--- scripts/mistnet.sample.drip.pl
+++ scripts/mistnet.sample.drip.pl
@@ -7,7 +7,7 @@ if (not defined $input_file) {
   $input_file = '/var/log/rkom21-lab/mistnet.sample';
 }
 
-while(true){
+while(1){
   open(my $fh, '<:encoding(UTF-8)', $input_file)
     or die "Could not open file '$input_file' $!";
 
