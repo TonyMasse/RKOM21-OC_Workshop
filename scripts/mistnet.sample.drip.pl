9f7f6fcd3a1d2455e642ec29fd0f7e3ab100dd60
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-01-25 16:34:24 +0000

    Update mistnet.sample.drip.pl
    Infinite Loop

--- scripts/mistnet.sample.drip.pl
+++ scripts/mistnet.sample.drip.pl
@@ -7,13 +7,14 @@ if (not defined $input_file) {
   $input_file = '/var/log/rkom21-lab/mistnet.sample';
 }
 
-open(my $fh, '<:encoding(UTF-8)', $input_file)
-  or die "Could not open file '$input_file' $!";
+while(true){
+  open(my $fh, '<:encoding(UTF-8)', $input_file)
+    or die "Could not open file '$input_file' $!";
 
-while(my $line = <$fh>) {
-  print $line;
-  sleep(1);
+  while(my $line = <$fh>) {
+    print $line;
+    sleep(1);
+  }
+
+  close $fh or die "Failed to close $input_file: $!\n";
 }
-
-close $fh or die "Failed to close $input_file: $!\n";
-
